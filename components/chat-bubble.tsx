97ad80a003f11878f8e4c860d230d81b7cf00774
jsen <senecal@hey.com>
2025-11-06 08:53:32 -0700

    wip bubble

---
+++ components/chat-bubble.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, View } from "react-native";
+
+export function ChatBubble() {
+    <View style={styles.bubble}>
+
+    </View>
+}
+
+const styles = StyleSheet.create({
+    bubble: {
+        backgroundColor: "#0078fe",
+        padding:10,
+        marginLeft: '45%',
+        borderRadius: 5,
+        //marginBottom: 15,
+        marginTop: 5,
+        marginRight: "5%",
+        maxWidth: '50%',
+        alignSelf: 'flex-end',
+        //maxWidth: 500,
+    }
+})
